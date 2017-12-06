bff3d3773acd7201cd837779f30bd8a222f26cfb
chicheong <chicheong@gmail.com>
2017-12-06 16:43:48 +0800

    Add Points,BloodPressure,Weight,Preferences

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,9 +1,17 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
+import { TwentyOnePointsPointsModule } from './points/points.module';
+import { TwentyOnePointsPreferencesModule } from './preferences/preferences.module';
+import { TwentyOnePointsWeightModule } from './weight/weight.module';
+import { TwentyOnePointsBloodPressureModule } from './blood-pressure/blood-pressure.module';
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
+        TwentyOnePointsPointsModule,
+        TwentyOnePointsPreferencesModule,
+        TwentyOnePointsWeightModule,
+        TwentyOnePointsBloodPressureModule,
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
